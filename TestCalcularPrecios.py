903598dc6051f67799a25bacf8949ac22852dc68
Carlos Daniel Gonzalez <carlos.dgonzalezc46@gmail.com>
2016-04-28 15:57:23 -0430

    commit again

--- TestCalcularPrecios.py
+++ TestCalcularPrecios.py
@@ -3,13 +3,21 @@ import calendar
 import sys
 import unittest
 
+
+formato = "%d/%m/%Y-%H:%M"
+
+class tarifa:
+    dias_semana = 0
+    fines_semana = 0
+    
 class TestCalcularPrecios(unittest.TestCase):
     def testtimedelta15min(self):
         tarifa = Tarifa()
         tarifa.dias_semana = 1
         tarifa.fines_semana = 1
-        ini = datetime.datetime(2016, 4, 1, 7, 0)
-        fin = datetime.datetime(2016, 4, 1, 7, 15)
+        
+        ini = datetime.strptime("24/04/2016-20:15", formato)
+        fin = datetime.strptime("24/04/2016-20:30", formato)
 
         self.assertEqual(1, calcularPrecio(tarifa, [ini, fin]))
         
